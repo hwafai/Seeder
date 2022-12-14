642e3f05d8d5e0168379dd7a7df7e805e62fde19
hwafai <54480317+hwafai@users.noreply.github.com>
2022-12-14 13:30:16 -0600

    Add files via upload

---
+++ seederUtils.js
@@ -0,0 +1,64 @@
+function convertToDecimal (otherSide) {
+    const newBase = otherSide / (1 - otherSide) + 1
+    return newBase
+  }
+  
+  function convertToPercent (price) {
+    if (price > 0) {
+      const percentOfBet = 1 / (price + 1)
+      console.log(percentOfBet)
+      return percentOfBet
+    } else {
+      price = Math.abs(price)
+      const percentOfBet = price / (price + 1)
+      return percentOfBet
+    }
+  }
+  
+  function convertDecimalToAmerican (decimalOdds) {
+    if (parseFloat(decimalOdds) > 101) {
+      return 10000
+    }
+    if (parseFloat(decimalOdds) === 2) {
+      return -100
+    }
+    if (parseFloat(decimalOdds) > 2.0) {
+      return (decimalOdds - 1) * 100
+    }
+    return -100 / (decimalOdds - 1)
+  }
+
+  function properOrders( type, number, gameID, side1, side2, seedAmount, newSeedA, secondNewA){
+    const firstOrder = {
+        gameID,
+        type,
+        side: side1,
+        bet: seedAmount,
+        odds: -1 * newSeedA,
+        expirationMinutes: 0
+    }
+    const comebackOrders = {
+        gameID,
+        type,
+        side: side2,
+        bet: seedAmount,
+        odds: -1 * secondNewA,
+        expirationMinutes: 0
+    }
+    if (type === 'spread'){
+        firstOrder.number = number
+        const secondNumber = -1 * number
+        comebackOrders.number = secondNumber
+    } else if (type === 'total'){
+        firstOrder.number = number
+        comebackOrders.number = number
+    }
+    return [firstOrder, comebackOrders]
+  }
+
+  module.exports = {
+    convertToDecimal,
+    convertToPercent,
+    convertDecimalToAmerican,
+    properOrders
+  }
