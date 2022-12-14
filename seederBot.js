05f300aab2b0430b3d3c0c8801c0989d9665d408
AwNike24 <38556753+AwNike24@users.noreply.github.com>
2022-12-14 14:13:28 -0600

    add a linter, clean up package json, refactor util function find other side, remove unused constants

--- seederBot.js
+++ seederBot.js
@@ -1,123 +1,158 @@
+require("./loadEnv");
+const { Manager } = require("socket.io-client");
+const callApi = require("./callApi");
 
-const { Manager } = require('socket.io-client')
-const callApi = require('./CallApi')
+const {
+  convertToDecimal,
+  convertToPercent,
+  convertDecimalToAmerican,
+  findOtherSide,
+  properOrders,
+} = require("./seederUtils");
 
-const { convertToDecimal, convertToPercent, convertDecimalToAmerican, properOrders } = require('./seederUtils')
-const url = `http://localhost:8080`
-
-function findOtherSide(participants, orderSide, type) {
-  if (type === 'moneyline' || type === 'spread') {
-    const participantIds = participants.map(p => p.id)
-    return participantIds.find(pId => pId !== orderSide)
-  }
-  return ['over', 'under'].find(side => side !== orderSide)
-}
+const url = process.env.FOURCASTER_API_URI;
+const wsUrl = process.env.FOURCASTER_WS_API_URI;
+const username = process.env.FOURCASTER_USERNAME;
+const password = process.env.FOURCASTER_PASSWORD;
 
 callApi({
   url: `${url}/user/login`,
-  method: 'POST',
+  method: "POST",
   data: {
-    username:'wafa12',
-    password:'Test1234'
-  }
-}).then((response) => {
-  const { user } = response.data
-    const username = user.username
-    const token = (user.auth)
-    const id = user.id
-    const runningUser = { username, id, token }
-    console.log(runningUser)
-    const manager = new Manager('ws://localhost:80', {
+    username,
+    password,
+  },
+})
+  .then((response) => {
+    const { user } = response.data;
+    const username = user.username;
+    const token = user.auth;
+    const id = user.id;
+    const runningUser = { username, id, token };
+    console.log(runningUser);
+    const manager = new Manager(wsUrl, {
       reconnectionDelayMax: 1000,
       query: {
-        token: token
-      }
-    })
+        token: token,
+      },
+    });
     const socket = manager.socket(`/v2/user/${username}`, {
       query: {
-        token: token
-      }
-    })
+        token: token,
+      },
+    });
 
-    socket.on('connect', () => {
-      console.log(`message: ${username} connected to userFeed`)
-    })
-    socket.on('positionUpdate', async (msg) => {
-      const formattedMessage = JSON.parse((msg))
-      if (!formattedMessage.unmatched){
-        console.log("user took offer on", formattedMessage.eventName, "for", formattedMessage.matched.risk, "on", formattedMessage.matched.type, formattedMessage.matched.number, "at", formattedMessage.matched.odds)
+    socket.on("connect", () => {
+      console.log(`message: ${username} connected to userFeed`);
+    });
+    socket.on("positionUpdate", async (msg) => {
+      const formattedMessage = JSON.parse(msg);
+      if (!formattedMessage.unmatched) {
+        console.log(
+          "user took offer on",
+          formattedMessage.eventName,
+          "for",
+          formattedMessage.matched.risk,
+          "on",
+          formattedMessage.matched.type,
+          formattedMessage.matched.number,
+          "at",
+          formattedMessage.matched.odds
+        );
       } else {
-        const gameID = formattedMessage.gameID
-        const DesiredVig = 0.04
-        const equityToLockIn = 0.01
-        const priceMove = DesiredVig - equityToLockIn
-        const orderAmount = formattedMessage.unmatched.offered
-        const odds = formattedMessage.unmatched.odds
-        const number = formattedMessage.unmatched.number
-        const type = formattedMessage.unmatched.type
-        const event = formattedMessage.eventName
-        const fillAmount = formattedMessage.unmatched.filled
-        const unFilled = formattedMessage.unmatched.remaining
-        const seedAmount = 100
-        const newSeeds = []
+        const gameID = formattedMessage.gameID;
+        const DesiredVig = 0.04;
+        const equityToLockIn = 0.01;
+        const priceMove = DesiredVig - equityToLockIn;
+        const orderAmount = formattedMessage.unmatched.offered;
+        const odds = formattedMessage.unmatched.odds;
+        const number = formattedMessage.unmatched.number;
+        const type = formattedMessage.unmatched.type;
+        const event = formattedMessage.eventName;
+        const fillAmount = formattedMessage.unmatched.filled;
+        const seedAmount = 100;
+        const newSeeds = [];
         if (formattedMessage.unmatched.filled === 0 && orderAmount > 0) {
-          console.log('user created offer on ', event, 'on', type, 'for', orderAmount, 'at', odds)
+          console.log(
+            "user created offer on ",
+            event,
+            "on",
+            type,
+            "for",
+            orderAmount,
+            "at",
+            odds
+          );
         } else if (orderAmount === 0) {
-          console.log('user canceled offer on', event, 'on', type, 'at', odds)
+          console.log("user canceled offer on", event, "on", type, "at", odds);
         } else {
-          console.log('user order on', event, 'matched for', fillAmount, 'on', type, 'at', odds)
-          const price = -1 * (odds / 100)
-          const percentOfBet = convertToPercent(price)
-          const roundedPercent = Math.round(percentOfBet * 100) / 100
-          const otherSide = roundedPercent + priceMove
-          const side1 = formattedMessage.unmatched.side
-          const secondSeed = (1 + (DesiredVig)) - otherSide
-          const newSeed = convertToDecimal(otherSide)
-          const newSeedA = -1 * (Math.round(convertDecimalToAmerican(newSeed)))
-          const secondNew = convertToDecimal(secondSeed)
-          const secondNewA = -1 * (Math.round(convertDecimalToAmerican(secondNew)))
-          newSeeds.push(newSeedA, secondNewA)
-          console.log('New Seed prices', newSeeds)
+          console.log(
+            "user order on",
+            event,
+            "matched for",
+            fillAmount,
+            "on",
+            type,
+            "at",
+            odds
+          );
+          const price = -1 * (odds / 100);
+          const percentOfBet = convertToPercent(price);
+          const roundedPercent = Math.round(percentOfBet * 100) / 100;
+          const otherSide = roundedPercent + priceMove;
+          const side1 = formattedMessage.unmatched.side;
+          const secondSeed = 1 + DesiredVig - otherSide;
+          const newSeed = convertToDecimal(otherSide);
+          const newSeedA = -1 * Math.round(convertDecimalToAmerican(newSeed));
+          const secondNew = convertToDecimal(secondSeed);
+          const secondNewA =
+            -1 * Math.round(convertDecimalToAmerican(secondNew));
+          newSeeds.push(newSeedA, secondNewA);
+          console.log("New Seed prices", newSeeds);
           await callApi({
             url: `${url}/session/cancelAllOrdersForGame`,
-            method: 'POST',
+            method: "POST",
             data: {
               gameID,
-              type
+              type,
             },
-            headers: { authorization: token }
-          })
+            headers: { authorization: token },
+          });
           const orderBook = await callApi({
             url: `${url}/exchange/v2/getOrderbook`,
-            method: 'GET',
+            method: "GET",
             params: {
-              gameID
+              gameID,
             },
-            headers: {authorization: token}
-          })
-          const orderParticipants = (orderBook.data.games[0].participants) 
-          console.log(orderParticipants) 
-          console.log(side1)
-          const side2 = findOtherSide(orderParticipants, side1, type)
-          console.log(type)
-          console.log(side2)
-          const orders = properOrders(type, number, gameID, side1, side2, seedAmount, newSeedA, secondNewA)
+            headers: { authorization: token },
+          });
+          const orderParticipants = orderBook.data.games[0].participants;
+          console.log(side1);
+          const side2 = findOtherSide(orderParticipants, side1, type);
+          console.log(side2);
+          const orders = properOrders(
+            type,
+            number,
+            gameID,
+            side1,
+            side2,
+            seedAmount,
+            newSeedA,
+            secondNewA
+          );
           await callApi({
             url: `${url}/session/v2/place`,
-            method: 'POST',
+            method: "POST",
             data: {
-                token: token,
-                orders,
-                gameID
-            }
-          })
+              token: token,
+              orders,
+              gameID,
+            },
+          });
         }
       }
-    })
-   
+    });
   })
   .catch(function (error) {
-    console.log(error)
-  })
-
-
+    console.log(error);
+  });
--- seederUtils.js
+++ seederUtils.js
@@ -1,64 +1,82 @@
-function convertToDecimal (otherSide) {
-    const newBase = otherSide / (1 - otherSide) + 1
-    return newBase
-  }
-  
-  function convertToPercent (price) {
-    if (price > 0) {
-      const percentOfBet = 1 / (price + 1)
-      console.log(percentOfBet)
-      return percentOfBet
-    } else {
-      price = Math.abs(price)
-      const percentOfBet = price / (price + 1)
-      return percentOfBet
-    }
-  }
-  
-  function convertDecimalToAmerican (decimalOdds) {
-    if (parseFloat(decimalOdds) > 101) {
-      return 10000
-    }
-    if (parseFloat(decimalOdds) === 2) {
-      return -100
-    }
-    if (parseFloat(decimalOdds) > 2.0) {
-      return (decimalOdds - 1) * 100
-    }
-    return -100 / (decimalOdds - 1)
-  }
+function convertToDecimal(otherSide) {
+  const newBase = otherSide / (1 - otherSide) + 1;
+  return newBase;
+}
 
-  function properOrders( type, number, gameID, side1, side2, seedAmount, newSeedA, secondNewA){
-    const firstOrder = {
-        gameID,
-        type,
-        side: side1,
-        bet: seedAmount,
-        odds: -1 * newSeedA,
-        expirationMinutes: 0
-    }
-    const comebackOrders = {
-        gameID,
-        type,
-        side: side2,
-        bet: seedAmount,
-        odds: -1 * secondNewA,
-        expirationMinutes: 0
-    }
-    if (type === 'spread'){
-        firstOrder.number = number
-        const secondNumber = -1 * number
-        comebackOrders.number = secondNumber
-    } else if (type === 'total'){
-        firstOrder.number = number
-        comebackOrders.number = number
-    }
-    return [firstOrder, comebackOrders]
+function convertToPercent(price) {
+  if (price > 0) {
+    const percentOfBet = 1 / (price + 1);
+    console.log(percentOfBet);
+    return percentOfBet;
+  } else {
+    price = Math.abs(price);
+    const percentOfBet = price / (price + 1);
+    return percentOfBet;
   }
+}
 
-  module.exports = {
-    convertToDecimal,
-    convertToPercent,
-    convertDecimalToAmerican,
-    properOrders
+function convertDecimalToAmerican(decimalOdds) {
+  if (parseFloat(decimalOdds) > 101) {
+    return 10000;
   }
+  if (parseFloat(decimalOdds) === 2) {
+    return -100;
+  }
+  if (parseFloat(decimalOdds) > 2.0) {
+    return (decimalOdds - 1) * 100;
+  }
+  return -100 / (decimalOdds - 1);
+}
+
+function findOtherSide(participants, orderSide, type) {
+  if (type === "moneyline" || type === "spread") {
+    const participantIds = participants.map((p) => p.id);
+    return participantIds.find((pId) => pId !== orderSide);
+  }
+  return ["over", "under"].find((side) => side !== orderSide);
+}
+
+function properOrders(
+  type,
+  number,
+  gameID,
+  side1,
+  side2,
+  seedAmount,
+  newSeedA,
+  secondNewA
+) {
+  const firstOrder = {
+    gameID,
+    type,
+    side: side1,
+    bet: seedAmount,
+    odds: -1 * newSeedA,
+    expirationMinutes: 0,
+  };
+  const comebackOrders = {
+    gameID,
+    type,
+    side: side2,
+    bet: seedAmount,
+    odds: -1 * secondNewA,
+    expirationMinutes: 0,
+  };
+  if (type === "spread") {
+    firstOrder.number = number;
+    const secondNumber = -1 * number;
+    comebackOrders.number = secondNumber;
+  } else if (type === "total") {
+    firstOrder.number = number;
+    comebackOrders.number = number;
+  }
+  return [firstOrder, comebackOrders];
+}
+
+module.exports = {
+  convertToDecimal,
+  convertToPercent,
+  convertDecimalToAmerican,
+  findOtherSide,
+  properOrders,
+};
