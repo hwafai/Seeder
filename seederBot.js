c1bd0fa97ce7b129c828aaa9a21644c8b0bb72de
AwNike24 <38556753+AwNike24@users.noreply.github.com>
2024-02-02 20:24:30 -0600

    fill threshold

--- seederBot.js
+++ seederBot.js
@@ -113,7 +113,7 @@ login(password, url, username)
         const type = formattedMessage.unmatched.type;
         const event = formattedMessage.eventName;
         const fillAmount = formattedMessage.unmatched.filled;
-        const fillThreshold = 0.495;
+        const fillThreshold = 0.32;
         if (formattedMessage.unmatched.filled === 0 && orderAmount > 0) {
           console.log(
             `${username} created offer on `,
